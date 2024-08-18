e24a46c7eddf776b0883199dac4fd6109cea7b42
ashraful1910 <ashraful1910@gmail.com>
2024-08-18 14:47:51 +0600

    plugin

---
+++ plugin/houzez-developer/houzez-developer.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: Houzez - Developer
+Plugin URI:  http://themeforest.net/user/themehjunction
+Description: Adds New Post Type Developer to Houzez Themes
+Version:     0.0.1
+Author:      Theme Junction
+Author URI:  http://themeforest.net/user/favethemes
+License:     GPL2
+*/
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
