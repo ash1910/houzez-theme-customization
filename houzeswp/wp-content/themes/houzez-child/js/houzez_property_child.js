632182b4ed0ec19e51759d99e5a8bb7bd4b4e4b4
ashraful1910 <ashraful1910@gmail.com>
2024-09-11 16:49:44 +0600

    attachment

--- houzeswp/wp-content/themes/houzez-child/js/houzez_property_child.js
+++ houzeswp/wp-content/themes/houzez-child/js/houzez_property_child.js
@@ -140,7 +140,7 @@ jQuery(document).ready( function($) {
 
             //Js for property attachments title_deed  upload
 
-            var atch_uploader = new plupload.Uploader({
+            var atch_uploader_title_deed = new plupload.Uploader({
                 browse_button: 'select_attachments_title_deed',
                 file_data_name: 'property_attachment_file',
                 url: ajax_url + "?action=houzez_property_attachment_upload&verify_nonce=" + verify_nonce,
@@ -152,9 +152,9 @@ jQuery(document).ready( function($) {
                     prevent_duplicates: true
                 }
             });
-            atch_uploader.init();
+            atch_uploader_title_deed.init();
 
-            atch_uploader.bind('FilesAdded', function(up, files) {
+            atch_uploader_title_deed.bind('FilesAdded', function(up, files) {
                 var houzez_thumbs = "";
                 var maxfiles = max_prop_attachments;
                 if(up.files.length > maxfiles ) {
@@ -167,19 +167,19 @@ jQuery(document).ready( function($) {
                 });
                 document.getElementById('houzez_attachments_title_deed_container').innerHTML += houzez_thumbs;
                 up.refresh();
-                atch_uploader.start();
+                atch_uploader_title_deed.start();
             });
 
 
-            atch_uploader.bind('UploadProgress', function(up, file) {
+            atch_uploader_title_deed.bind('UploadProgress', function(up, file) {
                 document.getElementById( "attachment-holder-" + file.id ).innerHTML = '<span>' + file.percent + "%</span>";
             });
 
-            atch_uploader.bind('Error', function( up, err ) {
+            atch_uploader_title_deed.bind('Error', function( up, err ) {
                 document.getElementById('houzez_atach_errors').innerHTML += "<br/>" + "Error #" + err.code + ": " + err.message;
             });
 
-            atch_uploader.bind('FileUploaded', function ( up, file, ajax_response ) {
+            atch_uploader_title_deed.bind('FileUploaded', function ( up, file, ajax_response ) {
                 var response = $.parseJSON( ajax_response.response );
 
                 if ( response.success ) {
@@ -198,6 +198,76 @@ jQuery(document).ready( function($) {
                             '<a class="btn btn-light-grey-outlined"><i class="houzez-icon icon-navigation-menu"></i></a>'+
                         '</td>'+
                         '<input type="hidden" class="propperty-attach-id" name="propperty_attachment_title_deed_ids[]" value="' + response.attachment_id + '"/>';
+
+                    document.getElementById( "attachment-holder-" + file.id ).innerHTML = attachment_file;
+
+                    propertyAttachmentEvents();
+
+                } else {
+                    console.log ( response );
+                }
+            });
+
+            //Js for property attachments passport  upload
+
+            var atch_uploader_passport = new plupload.Uploader({
+                browse_button: 'select_attachments_passport',
+                file_data_name: 'property_attachment_file',
+                url: ajax_url + "?action=houzez_property_attachment_upload&verify_nonce=" + verify_nonce,
+                filters: {
+                    mime_types : [
+                        { title : verify_file_type, extensions : "jpg,jpeg,png,pdf,zip" }
+                    ],
+                    max_file_size: attachment_max_file_size,
+                    prevent_duplicates: true
+                }
+            });
+            atch_uploader_passport.init();
+
+            atch_uploader_passport.bind('FilesAdded', function(up, files) {
+                var houzez_thumbs = "";
+                var maxfiles = max_prop_attachments;
+                if(up.files.length > maxfiles ) {
+                    up.splice(maxfiles);
+                    alert('no more than '+maxfiles + ' file(s)');
+                    return;
+                }
+                plupload.each(files, function(file) {
+                    houzez_thumbs += '<tr id="attachment-holder-' + file.id + '" class="attach-thumb">' + '' + '</tr>';
+                });
+                document.getElementById('houzez_attachments_passport_container').innerHTML += houzez_thumbs;
+                up.refresh();
+                atch_uploader_passport.start();
+            });
+
+
+            atch_uploader_passport.bind('UploadProgress', function(up, file) {
+                document.getElementById( "attachment-holder-" + file.id ).innerHTML = '<span>' + file.percent + "%</span>";
+            });
+
+            atch_uploader_passport.bind('Error', function( up, err ) {
+                document.getElementById('houzez_atach_errors').innerHTML += "<br/>" + "Error #" + err.code + ": " + err.message;
+            });
+
+            atch_uploader_passport.bind('FileUploaded', function ( up, file, ajax_response ) {
+                var response = $.parseJSON( ajax_response.response );
+
+                if ( response.success ) {
+
+                    var attachment_file = ''+
+                        '<td class="table-full-width table-cell-title">'+
+                            '<span>'+ response.attach_title +'</span>'+
+                        '</td>'+
+                        '<td>'+
+                            '<a href="'+ response.url +'" target="_blank" class="btn btn-light-grey-outlined"><i class="houzez-icon icon-download-bottom"></i></a>'+
+                        '</td>'+
+                        '<td>'+
+                            '<button data-meta_name="fave_attachments_passport" data-attach-id="' + 0 + '"  data-attachment-id="' + response.attachment_id + '" class="attachment-form-delete btn btn-light-grey-outlined"><i class="houzez-icon icon-close"></i></button>'+
+                        '</td>'+
+                        '<td class="sort-attachment">'+
+                            '<a class="btn btn-light-grey-outlined"><i class="houzez-icon icon-navigation-menu"></i></a>'+
+                        '</td>'+
+                        '<input type="hidden" class="propperty-attach-id" name="propperty_attachment_passport_ids[]" value="' + response.attachment_id + '"/>';
 
                     document.getElementById( "attachment-holder-" + file.id ).innerHTML = attachment_file;
 
--- houzeswp/wp-content/themes/houzez-child/template-parts/dashboard/submit/attachments.php
+++ houzeswp/wp-content/themes/houzez-child/template-parts/dashboard/submit/attachments.php
@@ -58,17 +58,17 @@
 	<!--  Form A  -->
 
 	<!--  title_deed  -->
-	<h2><?php echo houzez_option('cls_documents_title_deed', 'Form A'); ?></h2>
+	<h2><?php echo houzez_option('cls_documents_title_deed', 'Title Deed'); ?></h2>
 	<div class="dashboard-content-block">
-		<p><?php echo houzez_option('cl_decuments_title_deed_text', 'Upload Form A'); ?></p>
-		<p><a href="#" id="select_attachments_title_deed" class="btn btn-primary"><?php echo houzez_option('cl_attachment_title_deed_btn', 'Select Form A Attachment'); ?></a></p>
+		<p><?php echo houzez_option('cl_decuments_title_deed_text', 'Upload Title Deed'); ?></p>
+		<p><a href="#" id="select_attachments_title_deed" class="btn btn-primary"><?php echo houzez_option('cl_attachment_title_deed_btn', 'Select Title Deed Attachment'); ?></a></p>
 		<p id="houzez_atach_errors"></p>
 
 		<table class="dashboard-table draggable-table">
 			<thead>
 				<tr>
 					<td colspan="4">
-						<label><?php echo houzez_option('cl_uploaded_attachments_title_deed', 'Uploaded Form A Attachments'); ?></label>
+						<label><?php echo houzez_option('cl_uploaded_attachments_title_deed', 'Uploaded Title Deed Attachments'); ?></label>
 					</td>
 				</tr>
 			</thead>
@@ -111,6 +111,61 @@
 		
 	</div>
 	<!--  title_deed  -->
+
+	<!--  Passport  -->
+	<h2><?php echo houzez_option('cls_documents_passport', 'Passport'); ?></h2>
+	<div class="dashboard-content-block">
+		<p><?php echo houzez_option('cl_decuments_passport_text', 'Upload Passport'); ?></p>
+		<p><a href="#" id="select_attachments_passport" class="btn btn-primary"><?php echo houzez_option('cl_attachment_passport_btn', 'Select Passport Attachment'); ?></a></p>
+		<p id="houzez_atach_errors"></p>
+
+		<table class="dashboard-table draggable-table">
+			<thead>
+				<tr>
+					<td colspan="4">
+						<label><?php echo houzez_option('cl_uploaded_attachments_passport', 'Uploaded Passport Attachments'); ?></label>
+					</td>
+				</tr>
+			</thead>
+			<tbody id="houzez_attachments_passport_container" class="houzez_attachments_form_container">
+				<?php
+				if(houzez_edit_property()) { 
+					global $property_data;
+					$property_attachs = get_post_meta( $property_data->ID, 'fave_attachments_passport', false );
+                    $property_attachs = array_unique($property_attachs);
+
+                    if( !empty($property_attachs[0])) {
+                        foreach ($property_attachs as $prop_attach_id) {
+
+                            $fullimage_url  = wp_get_attachment_url( $prop_attach_id );
+                            $attachment_title = get_the_title($prop_attach_id);
+                            
+                            echo '<tr class="attach-thumb">
+								<td class="table-full-width table-cell-title">
+									<span>'.esc_attr($attachment_title).'</span>
+								</td>
+								<td>
+									<a href="'.$fullimage_url.'" target="_blank" class="btn btn-light-grey-outlined"><i class="houzez-icon icon-download-bottom"></i></a>
+								</td>
+								<td>
+									<button data-meta_name="fave_attachments_passport" data-attach-id="' . intval($property_data->ID) . '"  data-attachment-id="' . intval($prop_attach_id) . '" class="attachment-form-delete btn btn-light-grey-outlined"><i class="houzez-icon icon-close"></i></button>
+								</td>
+								<td class="sort-attachment">
+									<a class="btn btn-light-grey-outlined"><i class="houzez-icon icon-navigation-menu"></i></a>
+								</td>
+								<input type="hidden" class="propperty-attach-id" name="propperty_attachment_passport_ids[]" value="' . intval($prop_attach_id) . '"/>
+							</tr>';
+							
+
+                        }
+                    }
+				}
+				?>
+			</tbody>
+		</table>
+		
+	</div>
+	<!--  Passport  -->
 
 	<h2><?php echo houzez_option('cls_documents_other', 'Other Documents'); ?></h2>
 	<div class="dashboard-content-block">
@@ -164,4 +219,79 @@
 		</table>
 
 	</div><!-- dashboard-content-block -->
+
+	
+<?php
+$checked_verified_check1 = '';
+$checked_verified_check2 = '';
+$checked_verified_check3 = '';
+$checked_verified_check4 = '';
+$checked_verified_check5 = '';
+
+if (houzez_edit_property()) {
+    $verified_check1 = houzez_get_field_meta('verified_check1');
+	$verified_check2 = houzez_get_field_meta('verified_check2');
+	$verified_check3 = houzez_get_field_meta('verified_check3');
+	$verified_check4 = houzez_get_field_meta('verified_check4');
+	$verified_check5 = houzez_get_field_meta('verified_check5');
+
+    if( $verified_check1 ) {
+    	$checked_verified_check1 = 'checked';
+    }
+	if( $verified_check2 ) {
+    	$checked_verified_check2 = 'checked';
+    }
+	if( $verified_check3 ) {
+    	$checked_verified_check3 = 'checked';
+    }
+	if( $verified_check4 ) {
+    	$checked_verified_check4 = 'checked';
+    }
+	if( $verified_check5 ) {
+    	$checked_verified_check5 = 'checked';
+    }
+}
+?>
+
+
+	<h2><?php echo houzez_option('cls_guidelines', 'Guidelines'); ?></h2>
+	<div class="dashboard-content-block">
+		<p><?php echo houzez_option('cl_guidelines_text', 'Check the below guidelines to get varified batch.'); ?></p>
+
+		<label class="control control--checkbox hz-price-placeholder">
+			<input type="checkbox" id="verified_check1" name="verified_check1" <?php echo $checked_verified_check1; ?>>
+			<span class="control__indicator"></span>
+			<?php echo houzez_option('cl_verified_check1', 'All images are of supper quality'); ?>
+		</label>
+
+		<label class="control control--checkbox hz-price-placeholder">
+			<input type="checkbox" id="verified_check2" name="verified_check2" <?php echo $checked_verified_check2; ?>>
+			<span class="control__indicator"></span>
+			<?php echo houzez_option('cl_verified_check2', 'All images are genuine images of the property showing interior and exterior views'); ?>
+		</label>
+
+		<label class="control control--checkbox hz-price-placeholder">
+			<input type="checkbox" id="verified_check3" name="verified_check3" <?php echo $checked_verified_check3; ?>>
+			<span class="control__indicator"></span>
+			<?php echo houzez_option('cl_verified_check3', 'The Watermark is subtle and represents your company logo'); ?>
+		</label>
+
+		<label class="control control--checkbox hz-price-placeholder">
+			<input type="checkbox" id="verified_check4" name="verified_check4" <?php echo $checked_verified_check4; ?>>
+			<span class="control__indicator"></span>
+			<?php echo houzez_option('cl_verified_check4', 'Images have no footers with extra details'); ?>
+		</label>
+
+		<label class="control control--checkbox hz-price-placeholder">
+			<input type="checkbox" id="verified_check5" name="verified_check5" <?php echo $checked_verified_check5; ?>>
+			<span class="control__indicator"></span>
+			<?php echo houzez_option('cl_verified_check5', 'Images are not collages or include people'); ?>
+		</label>
+
+	</div>
+
+
+
+
+
 </div><!-- dashboard-content-block-wrap -->
