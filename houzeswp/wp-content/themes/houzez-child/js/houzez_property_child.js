736cb7377419e61c1d7c4e58167d806261d3aa6f
ash1910 <ashraful1910@gmail.com>
2024-09-11 11:47:39 +0600

    attachment

---
+++ houzeswp/wp-content/themes/houzez-child/js/houzez_property_child.js
@@ -0,0 +1,218 @@
+/**
+ * Created by waqasriaz on 06/10/15.
+ */
+jQuery(document).ready( function($) {
+    "use strict";
+
+    if ( typeof houzezProperty !== "undefined" ) {
+
+        var ajax_url = houzezProperty.ajaxURL;
+        var verify_nonce = houzezProperty.verify_nonce;
+        var verify_file_type = houzezProperty.verify_file_type;
+        var max_prop_attachments = houzezProperty.max_prop_attachments;
+        var attachment_max_file_size = houzezProperty.attachment_max_file_size;
+
+        /* ------------------------------------------------------------------------ */
+        /*  Property attachment1 Sort , delete
+         /* ------------------------------------------------------------------------ */
+        var propertyAttachmentEvents = function() {
+
+            $( ".houzez_attachments_form_container" ).sortable({
+                revert: 100,
+                placeholder: "attachments-placeholder",
+                handle: ".sort-attachment",
+                cursor: "move"
+            });
+
+            //Remove Image
+            $('.attachment-form-delete').on('click', function(e){
+                e.preventDefault();
+                var $this = $(this);
+                var thumbnail = $this.closest('.attach-thumb');
+                var loader = $this.siblings('.icon-loader');
+                var prop_id = $this.data('attach-id');
+                var thumb_id = $this.data('attachment-id');
+                var meta_name = $this.data('meta_name');
+
+                loader.show();
+
+                var ajax_request = $.ajax({
+                    type: 'post',
+                    url: ajax_url,
+                    dataType: 'json',
+                    data: {
+                        'action': 'houzez_remove_property_documents_form',
+                        'prop_id': prop_id,
+                        'thumb_id': thumb_id,
+                        'meta_name': meta_name,
+                        'removeNonce': verify_nonce
+                    }
+                });
+
+                ajax_request.done(function( response ) {
+                    if ( response.remove_attachment ) {
+                        thumbnail.remove();
+                    } else {
+
+                    }
+                });
+
+                ajax_request.fail(function( jqXHR, textStatus ) {
+                    alert( "Request failed: " + textStatus );
+                });
+
+            });
+
+        }
+        propertyAttachmentEvents();
+
+        //Js for property attachment1s upload
+        var houzez_property_attachments = function() {
+
+            var atch_uploader = new plupload.Uploader({
+                browse_button: 'select_attachments_form_a',
+                file_data_name: 'property_attachment_file',
+                url: ajax_url + "?action=houzez_property_attachment_upload&verify_nonce=" + verify_nonce,
+                filters: {
+                    mime_types : [
+                        { title : verify_file_type, extensions : "jpg,jpeg,png,pdf,zip" }
+                    ],
+                    max_file_size: attachment_max_file_size,
+                    prevent_duplicates: true
+                }
+            });
+            atch_uploader.init();
+
+            atch_uploader.bind('FilesAdded', function(up, files) {
+                var houzez_thumbs = "";
+                var maxfiles = max_prop_attachments;
+                if(up.files.length > maxfiles ) {
+                    up.splice(maxfiles);
+                    alert('no more than '+maxfiles + ' file(s)');
+                    return;
+                }
+                plupload.each(files, function(file) {
+                    houzez_thumbs += '<tr id="attachment-holder-' + file.id + '" class="attach-thumb">' + '' + '</tr>';
+                });
+                document.getElementById('houzez_attachments_form_a_container').innerHTML += houzez_thumbs;
+                up.refresh();
+                atch_uploader.start();
+            });
+
+
+            atch_uploader.bind('UploadProgress', function(up, file) {
+                document.getElementById( "attachment-holder-" + file.id ).innerHTML = '<span>' + file.percent + "%</span>";
+            });
+
+            atch_uploader.bind('Error', function( up, err ) {
+                document.getElementById('houzez_atach_errors').innerHTML += "<br/>" + "Error #" + err.code + ": " + err.message;
+            });
+
+            atch_uploader.bind('FileUploaded', function ( up, file, ajax_response ) {
+                var response = $.parseJSON( ajax_response.response );
+
+                if ( response.success ) {
+
+                    var attachment_file = ''+
+                        '<td class="table-full-width table-cell-title">'+
+                            '<span>'+ response.attach_title +'</span>'+
+                        '</td>'+
+                        '<td>'+
+                            '<a href="'+ response.url +'" target="_blank" class="btn btn-light-grey-outlined"><i class="houzez-icon icon-download-bottom"></i></a>'+
+                        '</td>'+
+                        '<td>'+
+                            '<button data-meta_name="fave_attachments_form_a" data-attach-id="' + 0 + '"  data-attachment-id="' + response.attachment_id + '" class="attachment-form-delete btn btn-light-grey-outlined"><i class="houzez-icon icon-close"></i></button>'+
+                        '</td>'+
+                        '<td class="sort-attachment">'+
+                            '<a class="btn btn-light-grey-outlined"><i class="houzez-icon icon-navigation-menu"></i></a>'+
+                        '</td>'+
+                        '<input type="hidden" class="propperty-attach-id" name="propperty_attachment_form_a_ids[]" value="' + response.attachment_id + '"/>';
+
+                    document.getElementById( "attachment-holder-" + file.id ).innerHTML = attachment_file;
+
+                    propertyAttachmentEvents();
+
+                } else {
+                    console.log ( response );
+                }
+            });
+
+
+            //Js for property attachments title_deed  upload
+
+            var atch_uploader = new plupload.Uploader({
+                browse_button: 'select_attachments_title_deed',
+                file_data_name: 'property_attachment_file',
+                url: ajax_url + "?action=houzez_property_attachment_upload&verify_nonce=" + verify_nonce,
+                filters: {
+                    mime_types : [
+                        { title : verify_file_type, extensions : "jpg,jpeg,png,pdf,zip" }
+                    ],
+                    max_file_size: attachment_max_file_size,
+                    prevent_duplicates: true
+                }
+            });
+            atch_uploader.init();
+
+            atch_uploader.bind('FilesAdded', function(up, files) {
+                var houzez_thumbs = "";
+                var maxfiles = max_prop_attachments;
+                if(up.files.length > maxfiles ) {
+                    up.splice(maxfiles);
+                    alert('no more than '+maxfiles + ' file(s)');
+                    return;
+                }
+                plupload.each(files, function(file) {
+                    houzez_thumbs += '<tr id="attachment-holder-' + file.id + '" class="attach-thumb">' + '' + '</tr>';
+                });
+                document.getElementById('houzez_attachments_title_deed_container').innerHTML += houzez_thumbs;
+                up.refresh();
+                atch_uploader.start();
+            });
+
+
+            atch_uploader.bind('UploadProgress', function(up, file) {
+                document.getElementById( "attachment-holder-" + file.id ).innerHTML = '<span>' + file.percent + "%</span>";
+            });
+
+            atch_uploader.bind('Error', function( up, err ) {
+                document.getElementById('houzez_atach_errors').innerHTML += "<br/>" + "Error #" + err.code + ": " + err.message;
+            });
+
+            atch_uploader.bind('FileUploaded', function ( up, file, ajax_response ) {
+                var response = $.parseJSON( ajax_response.response );
+
+                if ( response.success ) {
+
+                    var attachment_file = ''+
+                        '<td class="table-full-width table-cell-title">'+
+                            '<span>'+ response.attach_title +'</span>'+
+                        '</td>'+
+                        '<td>'+
+                            '<a href="'+ response.url +'" target="_blank" class="btn btn-light-grey-outlined"><i class="houzez-icon icon-download-bottom"></i></a>'+
+                        '</td>'+
+                        '<td>'+
+                            '<button data-meta_name="fave_attachments_title_deed" data-attach-id="' + 0 + '"  data-attachment-id="' + response.attachment_id + '" class="attachment-form-delete btn btn-light-grey-outlined"><i class="houzez-icon icon-close"></i></button>'+
+                        '</td>'+
+                        '<td class="sort-attachment">'+
+                            '<a class="btn btn-light-grey-outlined"><i class="houzez-icon icon-navigation-menu"></i></a>'+
+                        '</td>'+
+                        '<input type="hidden" class="propperty-attach-id" name="propperty_attachment_title_deed_ids[]" value="' + response.attachment_id + '"/>';
+
+                    document.getElementById( "attachment-holder-" + file.id ).innerHTML = attachment_file;
+
+                    propertyAttachmentEvents();
+
+                } else {
+                    console.log ( response );
+                }
+            });
+
+
+        }
+        houzez_property_attachments();
+
+
+    }
+
+});
--- houzeswp/wp-content/themes/houzez-child/template-parts/dashboard/submit/attachments.php
+++ houzeswp/wp-content/themes/houzez-child/template-parts/dashboard/submit/attachments.php
@@ -1,6 +1,118 @@
 <?php global $is_multi_steps; ?>
 <div id="attachments" class="dashboard-content-block-wrap <?php echo esc_attr($is_multi_steps);?>">
 	<h2><?php echo houzez_option('cls_documents', 'Property Documents'); ?></h2>
+
+	<!--  Form A  -->
+	<h2><?php echo houzez_option('cls_documents_form_a', 'Form A'); ?></h2>
+	<div class="dashboard-content-block">
+		<p><?php echo houzez_option('cl_decuments_form_a_text', 'Upload Form A'); ?></p>
+		<p><a href="#" id="select_attachments_form_a" class="btn btn-primary"><?php echo houzez_option('cl_attachment_form_a_btn', 'Select Form A Attachment'); ?></a></p>
+		<p id="houzez_atach_errors"></p>
+
+		<table class="dashboard-table draggable-table">
+			<thead>
+				<tr>
+					<td colspan="4">
+						<label><?php echo houzez_option('cl_uploaded_attachments_form_a', 'Uploaded Form A Attachments'); ?></label>
+					</td>
+				</tr>
+			</thead>
+			<tbody id="houzez_attachments_form_a_container" class="houzez_attachments_form_container">
+				<?php
+				if(houzez_edit_property()) { 
+					global $property_data;
+					$property_attachs = get_post_meta( $property_data->ID, 'fave_attachments_form_a', false );
+                    $property_attachs = array_unique($property_attachs);
+
+                    if( !empty($property_attachs[0])) {
+                        foreach ($property_attachs as $prop_attach_id) {
+
+                            $fullimage_url  = wp_get_attachment_url( $prop_attach_id );
+                            $attachment_title = get_the_title($prop_attach_id);
+                            
+                            echo '<tr class="attach-thumb">
+								<td class="table-full-width table-cell-title">
+									<span>'.esc_attr($attachment_title).'</span>
+								</td>
+								<td>
+									<a href="'.$fullimage_url.'" target="_blank" class="btn btn-light-grey-outlined"><i class="houzez-icon icon-download-bottom"></i></a>
+								</td>
+								<td>
+									<button data-meta_name="fave_attachments_form_a" data-attach-id="' . intval($property_data->ID) . '"  data-attachment-id="' . intval($prop_attach_id) . '" class="attachment-form-delete btn btn-light-grey-outlined"><i class="houzez-icon icon-close"></i></button>
+								</td>
+								<td class="sort-attachment">
+									<a class="btn btn-light-grey-outlined"><i class="houzez-icon icon-navigation-menu"></i></a>
+								</td>
+								<input type="hidden" class="propperty-attach-id" name="propperty_attachment_form_a_ids[]" value="' . intval($prop_attach_id) . '"/>
+							</tr>';
+							
+
+                        }
+                    }
+				}
+				?>
+			</tbody>
+		</table>
+		
+	</div>
+	<!--  Form A  -->
+
+	<!--  title_deed  -->
+	<h2><?php echo houzez_option('cls_documents_title_deed', 'Form A'); ?></h2>
+	<div class="dashboard-content-block">
+		<p><?php echo houzez_option('cl_decuments_title_deed_text', 'Upload Form A'); ?></p>
+		<p><a href="#" id="select_attachments_title_deed" class="btn btn-primary"><?php echo houzez_option('cl_attachment_title_deed_btn', 'Select Form A Attachment'); ?></a></p>
+		<p id="houzez_atach_errors"></p>
+
+		<table class="dashboard-table draggable-table">
+			<thead>
+				<tr>
+					<td colspan="4">
+						<label><?php echo houzez_option('cl_uploaded_attachments_title_deed', 'Uploaded Form A Attachments'); ?></label>
+					</td>
+				</tr>
+			</thead>
+			<tbody id="houzez_attachments_title_deed_container" class="houzez_attachments_form_container">
+				<?php
+				if(houzez_edit_property()) { 
+					global $property_data;
+					$property_attachs = get_post_meta( $property_data->ID, 'fave_attachments_title_deed', false );
+                    $property_attachs = array_unique($property_attachs);
+
+                    if( !empty($property_attachs[0])) {
+                        foreach ($property_attachs as $prop_attach_id) {
+
+                            $fullimage_url  = wp_get_attachment_url( $prop_attach_id );
+                            $attachment_title = get_the_title($prop_attach_id);
+                            
+                            echo '<tr class="attach-thumb">
+								<td class="table-full-width table-cell-title">
+									<span>'.esc_attr($attachment_title).'</span>
+								</td>
+								<td>
+									<a href="'.$fullimage_url.'" target="_blank" class="btn btn-light-grey-outlined"><i class="houzez-icon icon-download-bottom"></i></a>
+								</td>
+								<td>
+									<button data-meta_name="fave_attachments_title_deed" data-attach-id="' . intval($property_data->ID) . '"  data-attachment-id="' . intval($prop_attach_id) . '" class="attachment-form-delete btn btn-light-grey-outlined"><i class="houzez-icon icon-close"></i></button>
+								</td>
+								<td class="sort-attachment">
+									<a class="btn btn-light-grey-outlined"><i class="houzez-icon icon-navigation-menu"></i></a>
+								</td>
+								<input type="hidden" class="propperty-attach-id" name="propperty_attachment_title_deed_ids[]" value="' . intval($prop_attach_id) . '"/>
+							</tr>';
+							
+
+                        }
+                    }
+				}
+				?>
+			</tbody>
+		</table>
+		
+	</div>
+	<!--  title_deed  -->
+
+	<h2><?php echo houzez_option('cls_documents_other', 'Other Documents'); ?></h2>
 	<div class="dashboard-content-block">
 		<p><?php echo houzez_option('cl_decuments_text', 'You can attach PDF files, Map images OR other documents.'); ?></p>
 		<p><a href="#" id="select_attachments" class="btn btn-primary"><?php echo houzez_option('cl_attachment_btn', 'Select Attachment'); ?></a></p>
@@ -50,5 +162,6 @@
 				?>
 			</tbody>
 		</table>
+
 	</div><!-- dashboard-content-block -->
 </div><!-- dashboard-content-block-wrap -->
